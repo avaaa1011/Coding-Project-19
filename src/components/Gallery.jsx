5c75b42d9a67b3796f19886e441e7e02f1528a0d
Ava Tran <ava.tran1011@gmail.com>
2025-04-13 16:13:30 -0400

    went back and improved the code

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,66 +1,67 @@
 import React, {useEffect, useState} from "react";
 import TourCard from "./TourCard";
+const url = "https://course-api.com/react-tours-project"; 
 
-const Gallery = (tours, setTours, onRemove) => {
+const Gallery = (tours, setTours, onRemove) => 
+{
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
     //function that'll fetch data from the link
-    const fetchTours = async () => 
+    const fetchTours = async () =>{
+    try{
+        setLoading(true);
+        const response = await fetch(url);
+        //an error will show, if response isn't successful
+        if (!response.ok) {throw new Error("Failed to fetch tours");}
+        
+        const data = await response.json();
+        setTours(data);
+        }catch (error)
+        {console.log('Fetch error:', error);
+        setError(true);
+        }finally {
+        setLoading(false);
+        }};
+    
+    useEffect(() => {fetchTours();},[]);
+        
+    //will show data is still loading
+    if (loading) 
     {
-        try 
-        {
-            const response = await fetch("https://course-api.com/react-tours-project");
-            if (!response.ok) { throw new Error("Failed to fetch tours");}
-            const data = await response.json();
-
-            setTours(data); 
-        } catch (error) {
-            setError(true);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    //Calling the API
-    useEffect(() => 
-    {
-        fetchTours();
-    }, []); 
-
-    //Rendering loading state
-    if (loading) {
         return <h2>Loading...</h2>;
     }
-
-    //Rendering error state
-    if (error) {
-        return <h2>Something went wrong...</h2>;
+    
+    //error message will show if there's a problem with fetching data
+    if (error)
+    {
+        return <h2>Something went wrong.</h2>;
     }
-
-    //Rendering if there are no tours 
-    if (tours.length === 0) {
+          
+    //message + button will appear if there are no tours available 
+    if (tours.length === 0)
+    {
         return (
-            <div className="no-tours">
-                <h2>No Tours Available</h2>
-                <button onClick={fetchTours()} className="refresh-btn">Refresh</button>
-            </div>
+          <div className="no-tours">
+            <h2>No tours left</h2>
+            <button onClick={fetchTours}>
+              Refresh
+            </button>
+          </div>
         );
     }
-
-     //Rendering tour lists
-     return (
-        <section className="tour-list">
-            {tours.map((tour) => {
-                return (
-                    <TourCard
-                        key={tour.id} 
-                        {...tour} 
-                        onRemove={onRemove} />
-                );
-            })}
+    return (
+        <section className="gallery">
+          {tours.map((tour) => (
+            <TourCard
+            //rendering each tour with a unique key
+            key={tour.id}
+            {...tour}
+            onRemove={onRemove}
+            />
+          ))}
         </section>
-    );
+      );
 };
-
+    
 export default Gallery;
--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,29 +1,37 @@
-import {useState} from 'react';
 
-const TourCard = ({id, name, info, price, image, onRemove}) =>
-{
-    const [isReadMore, setIsReadMore] = useState(false);
- 
-    return 
-    (
-        <article className="tour-card">
-            {/* Tour Info */}
-            <h3>{name}</h3>
-            <img src={image} alt={name} className="tour-image" />
-            <p className="tour-price">${price}</p>
+import React, { useState } from "react"; //imports tools from React
 
-            <p className="tour-info">
-                {isReadMore ? info : `${info.substring(0, 50)}...`}
-                <button onClick={() => setIsReadMore(!isReadMore)} className="read-more-btn">
-                    {isReadMore ? "Show Less" : "Read More"}
-                </button>
-            </p>
-
-            {/* Button to remove the tour card */}
-            <button onClick={() => onRemove(id)} className="remove-btn">
-                Not Interested
-            </button>
-        </article>
-    )
+const TourCard = ({ id, name, info, image, price, onRemove }) => {
+  const [readMore, setReadMore] = useState(false);
+  
+  return (
+    <article className="tour-card">
+      {/* Tour images*/}
+      <img src={image} alt={name} className="tour-img" />
+      
+      <div className="tour-info">
+        <div className="tour-header">
+          {/* Tour Info: Name and Prices*/}
+          <h2>{name}</h2>
+          <h3 className="tour-price">${price}</h3>
+        </div>
+        
+        {/* More Tour Information is shown*/}
+        <p>
+          {readMore ? info : `${info.substring(0, 150)}...`}
+          {/* ShowMore Button for longer description  */}
+          <button className="read-more" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
+        
+        {/* Remove Button to remove tours  */}
+        <button className="not-interested" onClick={() => onRemove(id)}>
+          Not Interested
+        </button>
+      </div>
+    </article>
+  );
 }
-export default TourCard; 
+
+export default TourCard;
